162526e43362ac2f2873e31e3eb00b0bc7f08d87
Daniel Selga <danielselga@gmail.com>
2022-03-24 00:33:30 -0300

    chore: Liskov Substitution Principle

---
+++ SOLID/LiskovSubstitutionPrinciple.js
@@ -0,0 +1,18 @@
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  get area() {
+    return this.width * this.height;
+  }
+
+  toString() {
+    return `${this.width}x${this.height}`;
+  }
+}
+
+let rc = new Rectangle(2,3)
+
+console.log(rc.toString())
