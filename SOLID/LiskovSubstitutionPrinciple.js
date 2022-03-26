435c7008512e3627233918e11580b1e6fca22318
Daniel Selga | Growyx <danielselga@gmail.com>
2022-03-26 03:36:19 -0300

    feat: liskov substituition principle application

--- SOLID/LiskovSubstitutionPrinciple.js
+++ SOLID/LiskovSubstitutionPrinciple.js
@@ -1,26 +1,60 @@
 class Rectangle {
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  set width(value) {
+    this._width = value;
+  }
+
+  set height(value) {
+    this._height = value;
   }
 
   get area() {
-    return this.width * this.height;
+    return this._width * this._height;
   }
 
   toString() {
-    return `${this.width}x${this.height}`;
+    return `${this._width}x${this._height}`;
   }
 }
 
 class Square extends Rectangle {
-    constructor(size) {
-        super(size, size)
-    }
+  constructor(size) {
+    super(size, size);
+  }
+
+  set width(value) {
+    this._width = this._height = value;
+  }
+
+  set height(value) {
+    this._width = this._height = value;
+  }
 }
 
-let sq = new Square()
+let useIt = function (rc) {
+  let width = rc._width;
+  rc.height = 10
+  console.log(`Expected area of ${10*width} got ${rc.area}`)
+};
 
-let rc = new Rectangle(2,3)
+let sq = new Square(5);
+sq.width = 5;
+sq.height = 10;
+useIt(sq)
 
-console.log(rc.toString())
+let rc = new Rectangle(2, 3);
+useIt(rc)
+
+console.log(rc.toString());
