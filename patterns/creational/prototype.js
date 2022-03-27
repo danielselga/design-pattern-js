b099033949934e5d4d0186ea97bbc1df4a464667
Daniel Selga | Growyx <danielselga@gmail.com>
2022-03-27 02:05:38 -0300

    feature: prototype pattern summary

---
+++ patterns/creational/prototype.js
@@ -0,0 +1,8 @@
+/** PROTOTYPE PATTERN -> A partially  or fully initialized object that you copy (clone) and make use of.
+ * Complicated objects (e.g car) aren't designed from scratch.
+    * They reiterate existing deisgns.
+ * An existing (partially of fully constructed) design is a Prototype.
+ * We make a copy (clone) the prototype and customize it.
+    * Require deep copy support
+ * We make cloning convenient (e.g via a factory). 
+ */
