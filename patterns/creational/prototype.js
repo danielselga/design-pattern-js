bdefee53e5e591d352e751516e24ac0cc673cce4
Daniel Selga | Growyx <danielselga@gmail.com>
2022-03-28 22:35:33 -0300

    feat: copying objects throught serialization

--- patterns/creational/prototype.js
+++ patterns/creational/prototype.js
@@ -1,90 +1,89 @@
 /** PROTOTYPE PATTERN -> A partially  or fully initialized object that you copy (clone) and make use of.
  * Complicated objects (e.g car) aren't designed from scratch.
-    * They reiterate existing deisgns.
+ * They reiterate existing deisgns.
  * An existing (partially of fully constructed) design is a Prototype.
  * We make a copy (clone) the prototype and customize it.
-    * Require deep copy support
- * We make cloning convenient (e.g via a factory). 
+ * Require deep copy support
+ * We make cloning convenient (e.g via a factory).
  */
 
 class Address {
-    constructor(streetAddress, city, country) {
-        this.streetAddress = streetAddress
-        this.city = city
-        this.country = country
-    }
+  constructor(streetAddress, city, country) {
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.country = country;
+  }
 
-    deepCopy() {
-        return new Address(this.streetAddress, this.city, this.country)
-    }
+  deepCopy() {
+    return new Address(this.streetAddress, this.city, this.country);
+  }
 
-    toString() {
-        return `Address: ${this.streetAddress}, ${this.city}, ${this.country}`
-    }
+  toString() {
+    return `Address: ${this.streetAddress}, ${this.city}, ${this.country}`;
+  }
 }
 
 class Person {
-    constructor(name, address) {
-        this.name = name
-        this.address = address
-    }
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+  }
 
-    toString() {
-        return `${this.name} lives at ${this.address}`
-    }
+  toString() {
+    return `${this.name} lives at ${this.address}`;
+  }
 
-    greet() {
-        console.log(`Hi, my name is ${this.name} and i live at ${this.address}`)
-    }
+  greet() {
+    return `Hi, my name is ${this.name} and i live at ${this.address}`
+  }
 }
 
 class Serializer {
-    constructor(types) {
-        this.types  = types;
+  constructor(types) {
+    this.types = types;
+  }
+
+  markRecursive(object) {
+    // anoint each object with a type index
+    let idx = this.types.findIndex((t) => {
+      return t.name === object.constructor.name;
+    });
+    if (idx !== -1) {
+      object["typeIndex"] = idx;
+
+      for (let key in object) {
+        if (object.hasOwnProperty(key) && object[key] != null)
+          this.markRecursive(object[key]);
+      }
     }
+  }
 
-    markRecursive(object) {
-        let idx = this.types.findIndex(t => {
-            return t.name === object.constructor.name;
-        })
-
-        if (idx !== -1) {
-            object['typeIndex'] = idx
-
-            for(let key in object) {
-                if (object.hasOwnProperty(key)) {
-                    this.markRecursive(object[key])
-                }
-            }
+  reconstructRecursive(object) {
+    if (object.hasOwnProperty("typeIndex")) {
+      let type = this.types[object.typeIndex];
+      let obj = new type();
+      for (let key in object) {
+        if (object.hasOwnProperty(key) && object[key] != null) {
+          obj[key] = this.reconstructRecursive(object[key]);
         }
+      }
+      delete obj.typeIndex;
+      return obj;
     }
+    return object;
+  }
 
-    reconstructRecursive(object) {
-        if (object.hasOwnProperty('typeIndex')) {
-            const type = this.types[object.typeIndex]
-            const obj = new type()
-            for (let key in object) {
-                if (object.hasOwnProperty(key) && object[key] != null) {
-                    obj[key] = this.reconstructRecursive(object[key])
-                }
-            }
-            delete obj.typeIndex
-            return obj
-        }
-        return object
-    }
-
-    clone(object) {
-        this.markRecursive(object)
-        const copy = JSON.parse(JSON.stringify(object))
-        return this.reconstructRecursive(copy)
-    }
+  clone(object) {
+    this.markRecursive(object);
+    let copy = JSON.parse(JSON.stringify(object));
+    return this.reconstructRecursive(copy);
+  }
 }
 
-const john = new Person('john', new Address('123 London Road', 'London', 'UK'))
-let jane = john.deepCopy();
-jane.name = 'Jane';
-jane.address.streetAddress = '321 Angel St'; 
+const john = new Person("john", new Address("123 London Road", "London", "UK"));
+// jane.address.streetAddress = '321 Angel St';
+console.log(john.greet())
 
-console.log(john.toString()); 
-console.log(jane.toString());
+let s = new Serializer([Person, Address]);
+let jane = s.clone(john);
+console.log(jane.greet());
