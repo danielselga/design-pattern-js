cb5d966e222c0249e93695b28c0cbf02e7d0ae4b
Daniel Ferreira Selga <danielselga@gmail.com>
2022-04-05 23:36:18 -0300

    feat: chain of responsability summary

---
+++ src/patterns/behavioral/chainOfResponsability.js
@@ -0,0 +1,13 @@
+/** CHAIN OF RESPONSABILITY PATTERN -> A chain of components who all get a chance to process or a query, optionally having default processing implementation and an ability to terminate the procesing chain.
+ * Unethical behavior by an employee; who takes the blame?
+    * Employee.
+    * Manager.
+    * COE.
+ * You click a graphical element on a form:
+    * Button handles it, stops futher processing.
+    * Underlying group box.
+    * Underlying window.
+ * CCG computer game:
+    * Creature has atack and defense values.
+    * Those can be boosted by other cards.    
+ */
