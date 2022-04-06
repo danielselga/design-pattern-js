429c49db23e8072cd92ae4d7e416e440e68f9500
Daniel Ferreira Selga <danielselga@gmail.com>
2022-04-06 01:07:04 -0300

    feat: command pattern definition

---
+++ src/patterns/behavioral/command/command.js
@@ -0,0 +1,9 @@
+/** COMMAND PATTERN -> An object which represents an instruction to perform a particular action. Contains all the information necessary for the action to be taken.
+ * Ordinary statements are perishible.
+   * Cannot undo meber assignment.
+   * Cannot directly serialize sequence of actions (calls).
+ * Want an object that represents an operation.
+   * Person should change its age to value 22.
+   * Car should do explode()
+ * Uses: GUI commands, multi-level undo/redo, macro recording and more! 
+ */
