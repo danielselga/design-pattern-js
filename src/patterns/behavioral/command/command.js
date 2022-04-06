0b1199126497b4843990ead5c31948a161cad952
Daniel Ferreira Selga <danielselga@gmail.com>
2022-04-06 01:40:44 -0300

    feat: command pattern implemented

--- src/patterns/behavioral/command/command.js
+++ src/patterns/behavioral/command/command.js
@@ -7,3 +7,85 @@
    * Car should do explode()
  * Uses: GUI commands, multi-level undo/redo, macro recording and more! 
  */
+
+
+class BankAccount {
+   constructor(balance = 0) {
+      this.balance = balance
+   }
+
+   deposit(amount) {
+      this.balance += amount
+      console.log(`Deposited ${amout}, balance is now ${this.balance}`)
+   }
+
+   withDraw() {
+      if (this.balance - amount >= BankAccount.overdraftLimit) {
+         this.balance -= amount
+         console.log(`Withdew ${amount}, balance is now ${this.balance}`);
+         return true
+      }
+
+      return false
+   }
+
+   toString() {
+      return `Balance: ${this.balance}`
+   }
+}
+
+BankAccount.overdraftLimit = -500
+
+let Action = Object.freeze({
+   'deposit': 1,
+   'withdraw': 2
+})
+
+class BankAccountCommand {
+   constructor(account, action, amount) {
+      this.account = account
+      this.action = action
+      this.amount = amount
+      this.succeed = false
+   }
+
+   call() {
+      switch(this.action) {
+         case Action.deposit:
+            this.account.deposit(this.amount)
+            this.succeed = true
+            break;
+         case Action.withDraw:
+            this.suceed = this.account.withDraw(this.amount)
+            break;
+      }
+   }
+
+   undo() {
+      if (!this.succeed) {
+         return
+      }
+
+      switch(this.action) {
+         case Action.deposit:
+            this.account.withDraw(this.amount)
+            break;
+         case Action.withDraw:
+            this.account.deposit(this.amount)
+            break;
+      }
+   }
+}
+
+let ba = new BankAccount(100)
+ba.deposit(100)
+
+let cmd = new BankAccountCommand(ba, Action.deposit, 50)
+cmd.call()
+console.log(ba.toString())
+
+/** Summary
+ * Encapsulate all detatails of an operation in a separate object
+ * Define instruction for applyung the command (either in the command) itself, or elsewere.
+ * Optionally define composite commands
+ */
