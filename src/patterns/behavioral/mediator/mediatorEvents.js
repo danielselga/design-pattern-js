b8a1c6faf68a17b0ad52272b8498729340901bb3
Daniel Ferreira Selga <danielselga@gmail.com>
2022-04-10 20:21:54 -0300

    feat: mediator using events

---
+++ src/patterns/behavioral/mediator/mediatorEvents.js
@@ -0,0 +1,70 @@
+class Event {
+    constructor() {
+        this.handlers = new Map()
+        this.count = 0
+    }
+
+    subscribe(handler) {
+        this.handlers.set(this.count++, handler)
+        return this.count
+    }
+
+    unsubscribe(idx) {
+        this.handlers.delete(idx)
+    }
+
+    fire(sender, args) {
+        this.handlers.forEach(function(v,k) {
+            v(sender, args)
+        })
+    }
+}
+
+class PlayerScoredEventArgs {
+    constructor(playerName, goalsSocredSoFar) {
+        this.playerName = playerName,
+        this.goalsSocredSoFar = goalsSocredSoFar
+    }
+
+    print() {
+        console.log(`${this.playerName} has scored! their ${this.goalsSocredSoFar} goal`)
+    }
+}
+
+class Game {
+    constructor() {
+        this.events = new Event()
+    }
+}
+
+class Player {
+    constructor(name, game) {
+        this.name = name
+        this.game = game
+        this.goalsScored = 0
+    }
+
+    score() {
+        this.goalsScored++
+        let args = new PlayerScoredEventArgs(this.name, this.goalsScored)
+        this.game.events.fire(this, args)
+    }
+}
+
+class Coach {
+    constructor(game) {
+        game.events.subscribe(function(sender, args) {
+            if (args instanceof PlayerScoredEventArgs && args.goalsSocredSoFar < 3) {
+                console.log(`Coach says: well done, ${this.playerName}`)
+            }
+        })
+    }
+}
+
+const game = new Game()
+const player = new Player('sam', game)
+const coach = new Coach(game)
+
+player.score()
+player.score()
+player.score()
