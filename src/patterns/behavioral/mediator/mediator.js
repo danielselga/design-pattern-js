44f1c4f5d92c611dffe4f45482d1ab4944e0d8fb
Daniel Ferreira Selga <danielselga@gmail.com>
2022-04-10 20:11:31 -0300

    feat: mediator chatroom exemple

--- src/patterns/behavioral/mediator/mediator.js
+++ src/patterns/behavioral/mediator/mediator.js
@@ -6,3 +6,65 @@
     * Those references may go dead.
  * Solution: Have them all refer to some central component that facilitates communication.   
  */
+
+class Person {
+   constructor(name) {
+      this.name = name
+      this.chatLog = []
+   }
+
+   receive(sender, message) {
+      const s = `${sender}: ${message}`
+      this.chatLog.push(s)
+      console.log(`[${this.name}'s chat session] ${s}`)
+   }
+
+   say(message) {
+      this.room.broadcast(this.name, message)
+   }
+
+   pm(who, message) {
+      this.room.message(this.name, who, message)
+   }
+}
+
+class ChatRoom {
+   constructor() {
+      this.people = []
+   }
+
+   join(p) {
+      const joinMsg = `${p.name} joins the chat.`
+      this.broadcast('room', joinMsg)
+      p.room = this
+      this.people.push(p)
+   }
+
+   broadcast(source, message) {
+      for (let p of this.people) {
+         if (p.name !== source) {
+            p.receive(source, message)
+         }
+      }
+   }
+
+   message(source, destination, message) {
+      for (let p of this.people) {
+         if (p.name === destination) {
+            p.receive(source, message)
+         }
+      }
+   }
+}
+
+const room = new ChatRoom()
+const jhon = new Person('Jhon')
+const jane = new Person('Jane')
+
+jhon.say('Hi room!')
+jane.say('Hi Jhon!')
+
+const simon = new Person('Simon')
+simon.say('Hi! everyone.')
+
+jane.pm('Simon', 'Glad you could join us!')
