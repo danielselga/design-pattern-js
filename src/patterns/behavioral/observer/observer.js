c5387e3fe236374134edd03cfdbb186f7b209afc
Daniel Ferreira Selga <danielselga@gmail.com>
2022-04-10 22:40:30 -0300

    feat: observer pattern events

--- src/patterns/behavioral/observer/observer.js
+++ src/patterns/behavioral/observer/observer.js
@@ -1,8 +1,54 @@
-/** OBSERVER PATTERN -> An observer is an object that wished to be informed about events happening in the system. The entity generating the events is an observable.
- * We need to be informed when certain things happen.
-    * Objects property changes.
-    * Object does something.
-    * Some external event occours
- * We want to listen to events and be notified when they occour.
-    * Want to unsubscribe from events if we're no longer interested.  
- */
+class Event {
+    constructor() {
+        this.handlers = new Map();
+        this.count = 0;
+    }
+
+    subscribe(handler) {
+        this.handlers.set(++this.count, handler);
+        return this.count;
+    }
+
+    unsubscribe(idx) {
+        this.handlers.delete(idx);
+    }
+
+    // 1) who fired the event?
+    // 2) additional data (event args)
+    fire(sender, args) {
+        this.handlers.forEach(
+            (v, k) => v(sender, args)
+        );
+    }
+}
+
+class FallsIllArgs {
+    constructor(address) {
+        this.address = address;
+    }
+}
+
+class Person {
+    constructor(address) {
+        this.address = address;
+        this.fallsIll = new Event();
+    }
+
+    catchCold() {
+        this.fallsIll.fire(
+            this,
+            new FallsIllArgs(this.address)
+        );
+    }
+}
+
+let person = new Person('123 London Road');
+let sub = person.fallsIll.subscribe((s, a) => {
+    console.log(`A doctor has been called ` +
+        `to ${a.address}`);
+});
+person.catchCold();
+person.catchCold();
+
+person.fallsIll.unsubscribe(sub);
+person.catchCold();
